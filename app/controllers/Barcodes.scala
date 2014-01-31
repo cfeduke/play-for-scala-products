a6e12bcbebf88c31300706ec193a4b39e77a407b
Mike Slinn <mslinn@micronauticsresearch.com>
2014-01-31 01:50:24 -0800

    -

--- app/controllers/Barcodes.scala
+++ app/controllers/Barcodes.scala
@@ -24,8 +24,8 @@ object Barcodes extends Controller {
     import org.krysalis.barcode4j.output.bitmap.BitmapCanvasProvider
     import org.krysalis.barcode4j.impl.upcean.EAN13Bean
 
-    val output: ByteArrayOutputStream = new ByteArrayOutputStream
-    val canvas: BitmapCanvasProvider = new BitmapCanvasProvider(output, mimeType, ImageResolution, BufferedImage.TYPE_BYTE_BINARY, false, 0)
+    val output = new ByteArrayOutputStream
+    val canvas = new BitmapCanvasProvider(output, mimeType, ImageResolution, BufferedImage.TYPE_BYTE_BINARY, true, 0)
     val barcode = new EAN13Bean
     barcode.generateBarcode(canvas, ean)
     canvas.finish()
